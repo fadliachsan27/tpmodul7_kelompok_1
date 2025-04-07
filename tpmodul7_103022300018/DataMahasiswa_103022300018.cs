5b8c5d71fe97a577d4634bebfff7ec9bb1400bf8
fadliachsan27 <fadliachsan010@gmail.com>
2025-04-07 10:28:03 +0700

    Menambahkan class fadli_DataMahasiswa_103022300018

---
+++ tpmodul7_103022300018/DataMahasiswa_103022300018.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+public class DataMahasiswa_103022300018
+{
+    public class Mahasiswa
+    {
+        public string nama_depan { get; set; }
+        public string nama_belakang { get; set; }
+        public string nim { get; set; }
+        public string fakultas { get; set; }
+    }
+
+    public static void ReadJSON()
+    {
+        try
+        {
+            string jsonText = File.ReadAllText("tp7_1_103022300018.json");
+            Mahasiswa data = JsonSerializer.Deserialize<Mahasiswa>(jsonText);
+            Console.WriteLine($"Nama {data.nama_depan} {data.nama_belakang} dengan nim {data.nim} dari fakultas {data.fakultas}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Gagal membaca file JSON: " + ex.Message);
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        ReadJSON();
+    }
+}
